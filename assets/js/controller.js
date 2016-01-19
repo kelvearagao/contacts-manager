5f9c523f324f357597fe5722c4db5306542151b9
kelve <kelvearagao@gmail.com>
2016-01-19 12:55:14 -0300

    Desenvolvendo as rotas da aplicação.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,3 +1,38 @@
-angular.module('app', []).controller('AppCtrl', function($scope){
-	$scope.name = "World";
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider) {
+	// Define as Rotas
+	$routeProvider.when('/', {
+		controller: 'indexCtl',
+		templateUrl: 'assets/partials/index.html'
+	})
+
+	.when('/add-contact', {
+		controller: 'addCtl',
+		templateUrl: 'assets/partials/add.html'
+	})
+
+	.when('/contact/:id', {
+		controller: 'contactCtl',
+		templateUrl: 'assets/partials/contact.html'
+	})
+
+	.otherwise({
+		redirectTo: '/'
+	});
+
+	// Remove o símbolo '#' da URL
+	//$locationProvider.html5Mode({enabled: true,	requireBase: false});
+	$locationProvider.html5Mode(true);
 });
+
+// Define os controladores
+app.controller('indexCtl', function($scope){
+
+	})
+	.controller('addCtl', function($scope){
+
+	})
+	.controller('contactCtl', function($scope, $routeParams){
+		console.log($routeParams);
+	});
